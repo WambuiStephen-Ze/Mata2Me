6b05de45e9be11aedb08c708958175dda902f4ba
malcomkamau <malcomkamau504@gmail.com>
2025-07-18 09:39:50 +0300

    Using vanilla CSS instead

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center bg-white dark:bg-black">
-      <Text className="text-xl font-bold text-black dark:text-white">Welcome to Mata2Me</Text>
+    <View
+      style={[styles.home]}
+      className="flex-1 justify-center items-center bg-white dark:bg-black"
+    >
+      <Text className="text-xl font-bold text-black dark:text-white">Welcome to <Text style={[styles.brand]}>Mata2Me</Text></Text>
       <Text className="text-sm text-gray-700 dark:text-gray-300">Helping you get where you need to go</Text>
     </View>
   );
 }
+
+const accent = '#67d91bff'
+
+const styles = StyleSheet.create({
+  home: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  brand: {
+    color: accent,
+    fontWeight: 'bold'
+  },
+});
